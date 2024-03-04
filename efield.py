9d92959ee3dd552dc148feaaea0b9b66e42eee9e
Ali Rahimi <alrhim@amazon.com>
2024-03-04 10:07:32 -0800

    solve field between two parallel plates
    still need to compute the charge on these plates from the field.

--- efield.py
+++ efield.py
@@ -1,3 +1,6 @@
+from typing import List, NamedTuple
+
+import matplotlib.pylab as plt
 import torch
 from torch import nn
 
@@ -10,7 +13,7 @@ def potential_operator(
     if locations3d.shape[1] != 3:
         raise ValueError("Locations3d must be an Nx3 tensor")
 
-    D = nn.functional.pairwise_distance(locations3d, anchor_locations3d)
+    D = torch.cdist(locations3d.unsqueeze(0), anchor_locations3d.unsqueeze(0)).squeeze()
     return torch.exp(-anchor_widths * D**2)
 
 
@@ -24,6 +27,8 @@ def field_operator(
 
     If f(x) is the potential function (as implemented in forward()), this
     function returns df/(dx danchor_weights).
+
+    Returns a 3x len(locations3d) x len(anchor_locations) tensor.
     """
     k = potential_operator(locations3d, anchor_locations3d, anchor_widths)
 
@@ -32,7 +37,7 @@ def field_operator(
         -2
         * anchor_widths
         * k
-        * torch.dstack(
+        * torch.stack(
             (
                 locations3d[:, 0][:, None] - anchor_locations3d[:, 0][None, :],
                 locations3d[:, 1][:, None] - anchor_locations3d[:, 1][None, :],
@@ -51,7 +56,7 @@ class Potential(nn.Module):
         anchor_weights: torch.Tensor,
         anchor_widths: torch.Tensor,
     ):
-        super().__init__(self)
+        super().__init__()
         self.anchor_locations3d = nn.Parameter(anchor_locations3d)
         self.anchor_weights = nn.Parameter(anchor_weights)
         self.anchor_widths = nn.Parameter(anchor_widths)
@@ -59,6 +64,12 @@ class Potential(nn.Module):
     def forward(self, locations3d: torch.Tensor) -> torch.Tensor:
         return (
             potential_operator(locations3d, self.anchor_locations3d, self.anchor_widths)
+            @ self.anchor_weights
+        )
+
+    def field(self, locations3d: torch.Tensor) -> torch.Tensor:
+        return (
+            field_operator(locations3d, self.anchor_locations3d, self.anchor_widths)
             @ self.anchor_weights
         )
 
@@ -132,25 +143,89 @@ def test_pair_of_points():
 test_pair_of_points()
 
 
+class Box(NamedTuple):
+    xmin: float
+    xmax: float
+    ymin: float
+    ymax: float
+    zmin: float
+    zmax: float
+
+    def grid(self, nx: int, ny: int, nz: int) -> torch.Tensor:
+        return (
+            torch.stack(
+                torch.meshgrid(
+                    torch.linspace(self.xmin, self.xmax, nx),
+                    torch.linspace(self.ymin, self.ymax, ny),
+                    torch.linspace(self.zmin, self.zmax, nz),
+                    indexing="xy",
+                )
+            )
+            .reshape(3, -1)
+            .T
+        )
+
+
 def potential_function(
     test_locations3d: torch.Tensor,
     conductor_locations3d: torch.Tensor,
     conductor_potentials: torch.Tensor,
+    verbose=True,
 ) -> Potential:
     # Solve the constrained optimization problem
     #
     #   min_f \int_x ||d/dx f(x)||^2
     #   s.t.  f(x) = V(x) when for all x on the conductors.
 
-    anchor_width = 1.0
-    coeffs = minimum_norm_in_subspace(
-        torch.vstack(field_operator(test_locations3d, test_locations3d, anchor_width)),
-        potential_operator(conductor_locations3d, test_locations3d, anchor_width),
-        conductor_potentials,
-    )
+    class Result(NamedTuple):
+        field_energy: float
+        constraint_violation: float
+        potential: Potential
 
-    return Potential(test_locations3d, coeffs, anchor_width)
+    results: List[Result] = []
 
+    baseline_anchor_width = (torch.cdist(test_locations3d, test_locations3d)).mean()
+    for anchor_width in (torch.linspace(0.1, 1, 20) * baseline_anchor_width) ** 2:
+        # A 3*N x N matrix that maps coefficients to the electric field. The
+        # field is represented as a 3*N long vector.
+        DK = field_operator(test_locations3d, test_locations3d, anchor_width).reshape(
+            -1, len(test_locations3d)
+        )
+        # An M x N matrix that maps coefficients to voltage on the M conductors.
+        K = potential_operator(conductor_locations3d, test_locations3d, anchor_width)
 
-def grid_box3d(xs, ys, zs) -> torch.Tensor:
-    return torch.stack(torch.meshgrid(xs, ys, zs, indexing="xy")).reshape(3, -1).T
+        coeffs = minimum_norm_in_subspace(DK, K, conductor_potentials)
+
+        results.append(
+            Result(
+                torch.norm(DK @ coeffs).item(),
+                torch.norm(K @ coeffs - conductor_potentials).item()
+                / len(conductor_potentials),
+                Potential(test_locations3d, coeffs, anchor_width),
+            )
+        )
+
+    if verbose:
+        anchor_widths = [r.potential.anchor_widths.item() for r in results]
+
+        _, ax = plt.subplots(1, 1)
+
+        best_result = min(results)
+        ax.axhline(best_result.field_energy, ls="--", lw=1)
+        ax.axvline(best_result.potential.anchor_widths.item(), ls="--", lw=1)
+
+        ax.plot(anchor_widths, [r.field_energy for r in results], color="c")
+        ax.set_xlabel("anchor_width")
+        ax.set_ylabel("Field energy", color="c")
+        ax.tick_params(axis="y", labelcolor="c")
+
+        ax2 = ax.twinx()
+        ax2.plot(
+            anchor_widths,
+            [r.constraint_violation for r in results],
+            color="r",
+        )
+        ax2.set_ylabel("Constraing violation norm", color="r")
+        ax2.tick_params(axis="y", labelcolor="r")
+
+    return best_result.potential
